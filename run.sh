e2be448963bf209cd714a35971895494cdd3c777
Peng Zhou <zhoupengcv@sjtu.edu.cn>
2018-07-18 23:16:42 +0800

    Test ch3 useGeometry

--- run.sh
+++ run.sh
@@ -53,6 +53,42 @@ else
 ./useLK ../../data
 fi
 
+elif [ $1 = ch3useGeometry ]
+then
+# ./run.sh ch3useGeometry False Debug|Release
+buildPath="ch3/useGeometry/build"
+rebuild=$2
+buildType=$3
+make_build_dir $buildPath "$rebuild"
+cd $buildPath
+cmake -DCMAKE_BUILD_TYPE="$buildType" ..
+make
+
+if [ "$buildType" = Debug ]
+then
+gdbserver localhost:8080 ./eigenGeometry
+else
+./eigenGeometry
+fi
+
+elif [ $1 = ch3visualizeGeometry ]
+then
+# ./run.sh ch3visualizeGeometry False Debug|Release
+buildPath="ch3/visualizeGeometry/build"
+rebuild=$2
+buildType=$3
+make_build_dir $buildPath "$rebuild"
+cd $buildPath
+cmake -DCMAKE_BUILD_TYPE="$buildType" -DCMAKE_PREFIX_PATH="/home/shhs/Desktop/user/soft/Pangolin" ..
+make
+
+if [ "$buildType" = Debug ]
+then
+gdbserver localhost:8080 ./visualizeGeometry
+else
+./visualizeGeometry
+fi
+
 else
 echo "Pass"
 fi
